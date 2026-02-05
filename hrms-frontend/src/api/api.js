1b69319ed1643244b64ff24351deb1963c63bc98
monika0829 <monikamaurya0077@gmail.com>
2026-02-05 22:21:30 +0530

    Modified api.js file

--- hrms-frontend/src/api/api.js
+++ hrms-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://human-resource-management-system-b13f.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
